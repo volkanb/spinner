86c3a81b173180c522d95d0ab33ee9b16e45cb38
Volkan Benli <volkanbenli92@gmail.com>
2021-09-22 01:46:14 +0000

    Linting

--- spinner1.js
+++ spinner1.js
@@ -12,7 +12,7 @@ setTimeout(() => {
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\   ');
 }, 700);
 
 setTimeout(() => {
